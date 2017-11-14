aced80a0fde2dce5b408ba494f81189df57bf199
Wes Avedisian <Wes Avedisian@Chili_Ranch>
2017-11-13 22:24:43 -0500

    rhombus beginning

---
+++ Shapes/src/shapes/Rhombus.java
@@ -0,0 +1,14 @@
+package shapes;
+
+public class Rhombus extends Triangle
+{
+	private double side;
+	private double angle;
+	
+	public Rhombus(int side, double angle)
+	{
+		super(side);
+		super(angle);
+		
+	}
+}
